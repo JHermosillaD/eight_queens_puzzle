b5d953bafd246e11c7f1a5ff3312b1286296b7f9
Jesus Hermosilla <hermosilladiaz@gmail.com>
2024-03-14 06:06:29 -0600

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -9,6 +9,8 @@ import chess.svg
 '''
 tamaño_poblacion = int(input("Tamaño de la población: "))
 porcentaje_padres = int(input("Porcentaje de padres: "))
+porcentaje_cruza = int(input("Porcentaje de cruza: "))
+porcentaje_mutacion = int(input("Porcentaje de mutación: "))
 
 ''' Función que evalúa la aptitud de una solución
     E: solución <- Matriz de un tablero
@@ -54,7 +56,9 @@ def generarPoblacion(tamaño):
     S: padres <- Lista de soluciones padre  
 '''
 def seleccionarPadres(poblacion, aptitudes, porcentaje):
-    no_padres = math.ceil((tamaño_poblacion*porcentaje)/100)
+    no_padres = int((tamaño_poblacion*porcentaje)/100)
+    if ((no_padres % 2) != 0):
+        no_padres+=1
     padres = []
     aptitudes.sort(key=lambda a: a[1])
     i=0
@@ -72,7 +76,72 @@ def seleccionarPadres(poblacion, aptitudes, porcentaje):
         i+=1
     return padres
 
-ppl, appts = generarPoblacion(tamaño_poblacion)
-x = seleccionarPadres(ppl, appts, porcentaje_padres)
+def operadorCruza(solucion1, solucion2):    
+    first_half1  = solucion1[:,:4]
+    second_half1 = solucion1[:,4:]
+    first_half2  = solucion2[:,:4]
+    second_half2 = solucion2[:,4:]
+    sol_1 = np.concatenate((first_half1,second_half2),axis=1)
+    sol_2 = np.concatenate((first_half2,second_half1),axis=1)
+    return sol_1, sol_2
 
-print(x)
+def getRandomElement(solution, queen):
+    if (queen == True):
+        indxs = np.where(solution == 1)
+    else:
+        indxs = np.where(solution == 0)
+    rows = indxs[0]
+    cols = indxs[1]
+    idx = random.randint(0, len(rows)-1)
+    return rows[idx], cols[idx]
+
+def repairBoard(solution):
+    _, counts = np.unique(solution, return_counts=True)
+    while (counts[1] > 8):
+        row, col = getRandomElement(solution, queen=True)
+        solution[row][col] = 0
+        _, counts = np.unique(solution, return_counts=True)
+    while (counts[1] < 8):
+        row, col = getRandomElement(solution, queen=False)
+        solution[row][col] = 1
+        _, counts = np.unique(solution, return_counts=True)
+    return solution
+
+def mutateBoard(solution):
+    row_a, col_a = getRandomElement(solution, queen=True)
+    row_b, col_b = getRandomElement(solution, queen=False)
+    solution[row_a][col_a] = 0
+    solution[row_b][col_b] = 1
+    return solution
+
+# Inicializar población
+ppl, appts = generarPoblacion(tamaño_poblacion)
+
+# Cruzar padres y reparar hijos
+ppdrs = seleccionarPadres(ppl, appts, porcentaje_padres)
+hhjs = []    
+no_cruzadores = int((len(ppdrs*porcentaje_cruza)/100))
+if ((no_cruzadores % 2) != 0):
+    no_cruzadores+=1
+pares = list(range(0, no_cruzadores, 2))
+for i in pares:
+    hijo1, hijo2 = operadorCruza(ppdrs[i], ppdrs[i+1])
+    hijo1 = repairBoard(hijo1)
+    hijo2 = repairBoard(hijo2)
+    hhjs.append(hijo1)
+    hhjs.append(hijo2)
+
+# Mutar hijos
+no_mutados = int((len(hhjs*porcentaje_mutacion)/100))
+for i in range(no_mutados):
+    hhjs[i] = mutateBoard(hhjs[i])
+
+# Actualizar población
+ppl = ppl + hhjs
+nuevas_apptitudes = []
+for index in range(len(ppl)):
+        aptitud = calcularAptitud(ppl[index])
+        nuevas_apptitudes.append((index,aptitud))
+nuevas_apptitudes.sort(key=lambda a: a[1])
+nuevas_apptitudes = nuevas_apptitudes[:tamaño_poblacion] 
+print(nuevas_apptitudes)
